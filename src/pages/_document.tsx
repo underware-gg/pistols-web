f0cbf9fea9a9e6a19e5fff9060d49f02752a9f73
FortunaRegem <mario2000959@gmail.com>
2025-05-05 18:54:45 +0200

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Single SVG favicon */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <meta name="theme-color" content="#0347AD" />
+      </Head>
       <body>
         <Main />
         <NextScript />
