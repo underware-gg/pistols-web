da1bfa45efdb9f69a43b8e485bc25b9f8bceb867
FortunaRegem <mario2000959@gmail.com>
2025-05-26 11:03:26 +0200

    Added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,29 @@ import '/src/styles/fonts.scss'
 import '/src/styles/styles.scss'
 import React from 'react'
 import { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function _app({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-24V4JK1LL7"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-24V4JK1LL7');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
   )
 }
 
